9214a73e7e401cf3c4c6cf3dcd2fbc00a1a773e6
shahbaz4783 <shahbaz4783@gmail.com>
2024-05-05 21:53:42 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+		<main className='flex min-h-screen space-y-8 flex-col items-center justify-between p-8'>
 			<h1 className='text-3xl font-semibold'>A Hub of Utilities and Tools</h1>
-			<section className='grid gap-4 grid-cols-2 sm:grid-cols-4'>
+			<section className='grid gap-4 md:grid-cols-4'>
 				{tools.map((item, index) => (
 					<Link key={index} href={item.path} className='border rounded-sm p-3'>
 						<h2 className='text-lg'>{item.title}</h2>
